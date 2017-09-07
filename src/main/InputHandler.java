51833a96dccdcf86515d2149bb2153398ca3c751
Bastiaan Mevius <meviusb@hotmail.com>
2017-09-07 11:28:39 +0200

    Added in and output files

---
+++ src/main/InputHandler.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author s147889
+ */
+public class InputHandler {
+    Scanner scanner;
+    String input;
+
+    public InputHandler() {
+        scanner = new Scanner(System.in);
+        
+        createInput(scanner.nextLine());
+    }
+
+    private void createInput(String nextLine) {
+        
+    }
+}
---
+++ src/main/OutputHandler.java
@@ -0,0 +1,13 @@
+package main;
+
+/**
+ *
+ * @author s147889
+ */
+public class OutputHandler {
+    String output;
+
+    public OutputHandler(String output) {
+        this.output = output;
+    }
+}
