cdc214ce02dd285c5f9cf38435c0c09c994d9cdf
Jorrit Olthuis <j.j.olthuis@student.tue.nl>
2017-09-07 09:48:38 +0200

    Add operations

---
+++ src/main/Add.java
@@ -0,0 +1,17 @@
+package main;
+
+/**
+ *
+ * @author s157710
+ */
+public class Add extends Operation {
+
+    public Add(BigInt x, BigInt y) {
+        super(x, y);
+    }
+
+    @Override
+    public BigInt compute(){
+        return null;
+    }
+}
