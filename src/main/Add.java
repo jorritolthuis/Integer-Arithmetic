34d06fc525d0d5c41109b111f8aeab2b7941c97e
D.M <d.a.w.markus@student.tue.nl>
2017-09-08 17:01:12 +0200

    made the return function and changed the code to add two numbers of differt sizes (still only for positive numbers)

--- src/main/Add.java
+++ src/main/Add.java
@@ -4,16 +4,20 @@ public class Add extends Operation {
 
     public Add(BigInt x, BigInt y) {
         super(x, y);
+    }
+
+    @Override
+    public BigInt compute(){
         int xLength = x.val.length();
         int yLength = y.val.length();
-        int maxLength = Math.max(xLength,yLength);
+        int minLength = Math.min(xLength,yLength);
         int carry = 0;
         int xi =0;
         int yi=0;
         BigInt z = new BigInt("",x.isPositive,x.rad);
         z.rad = x.rad;
         int t;
-        for(int i =0; i<maxLength;i++)
+        for(int i =0; i<minLength;i++)
         {
             xi = Integer.parseInt((Character.toString(x.val.charAt(xLength-1-i))),x.rad);
             yi = Integer.parseInt((Character.toString(x.val.charAt(yLength-1-i))),x.rad);
@@ -30,15 +34,27 @@ public class Add extends Operation {
             }
             z.val = Integer.toHexString(t)+z.val; 
         }
-        if(carry == 1)
+        if(carry == 1 && xLength != minLength)
         {
-            z.val = Integer.toHexString(carry)+z.val;
+            z.val = Integer.toHexString(Integer.parseInt(Character.toString(x.val.charAt(xLength-yLength-1)))+carry)+z.val; //setting number right with carry
+            if(xLength-yLength>1) //checking wheter part of number just have to be copied
+            {
+                z.val = x.val.substring(0,xLength-yLength-1)+z.val;
+            }
         }
-
-    }
-
-    @Override
-    public BigInt compute(){
-        return null;
+        
+        else if(carry == 1 && yLength != minLength)
+        {
+            z.val = Integer.toHexString(Integer.parseInt(Character.toString(y.val.charAt(yLength-xLength-1)))+carry)+z.val; //setting number right with carry
+            if(yLength-xLength>1) //checking wheter part of number just have to be copied
+            {
+                z.val = y.val.substring(0,yLength-xLength-1)+z.val;
+            }
+        }
+        else if (carry == 1 && yLength == xLength )
+        {
+            z.val = Integer.toHexString(carry) + z.val;
+        }
+        return z;
     }
 }
